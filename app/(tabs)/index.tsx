6a628d27d6bfe26b674516ad95c8b74ab90dee3f
idho dev <idhosembiring6@gmail.com>
2026-03-11 19:44:06 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,114 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-
+import { View, Text, Image, StyleSheet,ImageBackground  } from 'react-native';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [fontsLoaded] = useFonts({
+  Poppins_400Regular,
+  Poppins_700Bold,
+});
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+if (!fontsLoaded) {
+  return null;
+}
+  return (
+        <View style={styles.container}>
+      
+      <Image 
+        source={require("../../assets/images/photo_profile.jpeg")}
+        style={styles.profilePic} 
+      />
+
+      <Text style={styles.name}>IDHO JONATHAN SEMBIRING</Text>
+      <Text style={styles.nim}>NIM: 243303621251</Text>
+      <Text style={styles.jurusan}>Sistem Informasi</Text>
+
+      <View style={styles.Card}>
+        <Text style={styles.motivation}>
+          “According to aerodynamics, the bumblebee shouldn’t be able to fly.
+          Its wings are too small to get its fat little body off the ground.
+          But the bee flies anyway.”
+        </Text>
+      </View>
+
+      <Text style={styles.bioText}>
+        🚀 Currently mastering React Native SDK 50.
+        Goal: Build goated apps!
+      </Text>
+
+    </View>
+    
+    
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+ 
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    padding: 20,
+    
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  profilePic: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    borderWidth: 3,
+    borderColor: '#ffff99',
+    marginBottom: 15,
+
+    shadowColor: '#ffff99',
+    shadowOffset: { width: 0, height: 0 },
+    elevation: 40,
+    
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  name: {
+    fontSize: 24,
+    fontWeight: '900',
+    color: '#fff',
+    marginBottom: 5,
+    fontFamily: "Poppins_700Bold",
+  },
+  nim: {
+    fontSize: 14,
+    color: '#ffff99',
+    marginBottom: 20,
+    letterSpacing: 2,
+    fontFamily: "Poppins_700Bold",
+  },
+  jurusan: {
+    fontSize: 20,
+    color: '#ffffff',
+    marginBottom: 20,
+    letterSpacing: 2,
+    fontFamily: "Poppins_700Bold",
+    textShadowColor: '#ffffff',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 9,
+  },
+  motivation: {
+    fontSize: 12,
+    color: '#ffffff',
+    letterSpacing: 1,
+    fontFamily: "Poppins_700Bold",
+    textAlign: "center"
+  },
+  Card: {
+    backgroundColor: '#111',
+    padding: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#333',
+    width: '100%',
+    marginBottom: 15,
+    shadowColor: '#ffffff',
+    shadowOffset: { width: 0, height: 0 },
+    elevation: 5,
+    
+  },
+  bioText: {
+    color: '#ccc',
+    textAlign: 'center',
+    fontStyle: 'italic',
   },
 });
